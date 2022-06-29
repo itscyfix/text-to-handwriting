b9ff9b89a9d39dbbffea063c78b4b657dea7731c
Kanav Miglani <108290801+itscyfix@users.noreply.github.com>
2022-06-29 16:18:19 +0530

    spelling mistake

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import time
 
 print("welcome to text to handwriting made by me(kanav/cyfix) you have 2 colors to chose from (red/blue) it may be helpful to those who want to copy and paste their school assignment lmao ")
 txt = input("enter the text here : ")
-colorr = int(input("what shoulf be the color of pen(blue/red) 1 for blue or 0 for red  :  "))
+colorr = int(input("what should be the color of pen(blue/red) 1 for blue or 0 for red  :  "))
 if colorr == 1:
     colorr = [0,0,138]
 else:
